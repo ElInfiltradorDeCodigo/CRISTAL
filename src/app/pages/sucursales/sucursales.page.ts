193a77396c8c082177a39e0793c00d9533d5c07d
ElInfiltradorDeCodigo <perrokasper2003@gmail.com>
2023-10-15 17:45:17 -0600

    Implementación de metodos de eliminacion a Sucursales

--- src/app/pages/sucursales/sucursales.page.ts
+++ src/app/pages/sucursales/sucursales.page.ts
@@ -3,7 +3,9 @@ import { ActionSheetController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sucursales',
@@ -15,9 +17,10 @@ export class SucursalesPage implements OnInit {
   sucursales$!: Observable<any[]>;
 
   constructor(private actionSheetCtrl: ActionSheetController, private router: Router,
-               private alertController: AlertController, private db: AngularFireDatabase) { }
+               private alertController: AlertController, private db: AngularFireDatabase,
+               private storage: AngularFireStorage, private toastController: ToastController) { }
 
-  async presentActionSheet() {
+    async presentActionSheet(sucursal: any) {
     const actionSheet = await this.actionSheetCtrl.create({
       header: 'Opciones De Sucursal',
       backdropDismiss: false,
@@ -29,7 +32,7 @@ export class SucursalesPage implements OnInit {
             action: 'delete',
           },
           handler: () => {
-            this.eliminacion();
+            this.eliminacion(sucursal);
         },
       },
         {
@@ -63,12 +66,48 @@ export class SucursalesPage implements OnInit {
     await actionSheet.present();
   }
 
-    async eliminacion(){
-
+  async eliminacion(sucursal: any){
     const alert = await this.alertController.create({
       header: '¡Advertencia!',
       message: '¿Realmente desea eliminar esta sucursal?',
-      buttons: ['Sí','No'],
+      buttons: [
+        {
+          text: 'Sí',
+          handler: () => {
+            
+            this.storage.refFromURL(sucursal.imageUrl).delete().subscribe(() => {
+              console.log("Imagen eliminada del storage");
+            }, error => {
+              console.error("Error eliminando imagen del storage", error);
+            });
+  
+            this.db.object(`SUCURSALES/${sucursal.key}`).remove().then(async () => {
+              console.log("Sucursal eliminada de la base de datos");
+
+              const toast = await this.toastController.create({
+                message: 'Sucursal eliminada con éxito.',
+                duration: 2000,
+                color: 'success'
+            });
+            toast.present();
+            
+            }).catch(async error => {
+              console.error("Error eliminando sucursal de la base de datos", error);
+
+              const toast = await this.toastController.create({
+                message: 'Error eliminando sucursal. Por favor, intenta de nuevo.',
+                duration: 2000,
+                color: 'danger'
+            });
+            toast.present();
+            });
+          }
+        },
+        {
+          text: 'No',
+          role: 'cancel'
+        }
+      ]
     });
 
     await alert.present();
@@ -88,9 +127,11 @@ export class SucursalesPage implements OnInit {
   }
 
   ngOnInit() {
-
-    this.sucursales$ = this.db.list('SUCURSALES').valueChanges();
-
-  }
+    this.sucursales$ = this.db.list('SUCURSALES').snapshotChanges().pipe(
+      map(changes => 
+        changes.map(c => ({ key: c.payload.key, ...(c.payload.val() as object) }))
+      )
+    );
+  }  
 
 }
