f2d46c923aa3355e0b2c45e20dccb4b2b294e502
ElInfiltradorDeCodigo <perrokasper2003@gmail.com>
2023-10-18 17:48:54 -0600

    Implemetacion de Metodo Eliminar en Productores

--- src/app/pages/clientes/clientes.page.ts
+++ src/app/pages/clientes/clientes.page.ts
@@ -3,7 +3,9 @@ import { ActionSheetController } from '@ionic/angular';
 import { Router } from '@angular/router';
 import { AlertController } from '@ionic/angular';
 import { AngularFireDatabase } from '@angular/fire/compat/database';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-clientes',
@@ -13,11 +15,15 @@ import { Observable } from 'rxjs';
 export class ClientesPage implements OnInit {
 
   productores!: Observable<any[]>;
+  private _productores = new BehaviorSubject<any[]>([]);
+  selectedProductor: any;
 
   constructor(private actionSheetCtrl: ActionSheetController, private router: Router,
-    private alertController: AlertController, private db: AngularFireDatabase,) { }
+    private alertController: AlertController, private db: AngularFireDatabase, private storage: AngularFireStorage,
+    private toastCtrl: ToastController) { }
 
-  async presentActionSheet() {
+    async presentActionSheet(productor: any) {
+    this.selectedProductor = productor;
     const actionSheet = await this.actionSheetCtrl.create({
       header: 'Opciones De Productor',
       backdropDismiss: false,
@@ -63,17 +69,36 @@ export class ClientesPage implements OnInit {
     await actionSheet.present();
   }
 
-    async eliminacion(){
+  async showToast(message: string, color: 'success' | 'danger' = 'success') {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: 2000,
+      color: color,
+    });
+    toast.present();
+  }  
 
+  async eliminacion() {
+    if (!this.selectedProductor) {
+        console.error('selectedProductor no está definido.');
+        return;
+    }
     const alert = await this.alertController.create({
-      header: '¡Advertencia!',
-      message: '¿Realmente desea eliminar este productor?',
-      buttons: ['Sí','No'],
+        header: '¡Advertencia!',
+        message: '¿Realmente desea eliminar este productor?',
+        buttons: [
+            {
+                text: 'Sí',
+                handler: () => {
+                    this.eliminarProductor(this.selectedProductor.key, this.selectedProductor.imageUrl);
+                }
+            },
+            'No'
+        ],
     });
 
     await alert.present();
-
-  }
+}
 
   verPerfil(){
 
@@ -95,7 +120,33 @@ export class ClientesPage implements OnInit {
   }
 
   cargarProductores() {
-    this.productores = this.db.list('PRODUCTORES').valueChanges();
+    this.db.list('PRODUCTORES').snapshotChanges().subscribe(productoresSnap => {
+      const productoresData = productoresSnap.map(productor => {
+        return {
+          key: productor.key,
+          ...productor.payload.val() as any
+        }
+      });
+      this._productores.next(productoresData);
+    });
+
+    this.productores = this._productores.asObservable();
+}
+
+async eliminarProductor(productorKey: string, imageUrl: string) {
+  try {
+      
+      const imageRef = this.storage.refFromURL(imageUrl);
+      await imageRef.delete().toPromise();
+      
+      await this.db.object(`PRODUCTORES/${productorKey}`).remove();
+      
+      this.showToast("Productor eliminado con éxito", 'success');
+      
+  } catch (error) {
+      console.error("Error al eliminar productor: ", error);
+      this.showToast("Error al eliminar productor", 'danger');
   }
+}
 
 }
