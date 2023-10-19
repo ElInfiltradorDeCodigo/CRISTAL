cf4bda11a5f1511f2e80bb63e9996f4df9d9cd98
ElInfiltradorDeCodigo <perrokasper2003@gmail.com>
2023-10-19 13:48:55 -0600

    Mejoras: La Grafica ya se alimenta de Ventas

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -1,4 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/compat/database';
+
+interface Venta {
+  empleado: string;
+  fecha: string;
+  nombre_cliente: Cliente;
+  productos: Producto[];
+  telefono_cliente: string;
+  total: string;
+}
+
+interface Cliente {
+  apellido_m: string;
+  apellido_p: string;
+  estado: string;
+  imageUrl: string;
+  localidad: string;
+  nombre: string;
+  telefono: string;
+}
+
+interface Producto {
+  cantidad: string;
+  nombre: string;
+  precio: number;
+}
 
 @Component({
   selector: 'app-inicio',
@@ -7,27 +33,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioPage implements OnInit {
 
+  private COLORS = [
+    'rgba(255, 99, 132, 0.5)',
+    'rgba(54, 162, 235, 0.5)',
+    'rgba(255, 206, 86, 0.5)',
+  ];
+
   public barChartOptions = {
     scaleShowVerticalLines: false,
     responsive: true,
   };
 
-  public barChartLabels = ['Cuchillas R-2', 'Cuchillas R-1', 'Canaletas', 'Acido Formico', 'Alambre Cal. 12'];
+  public barChartLabels: string[] = [];
   public barChartType = 'bar';
   public barChartLegend = true;
-  public barChartData = [
-    { data: [108, 98, 500, 84, 250], label: 'Cantidad En Base A La Unidad Medida', backgroundColor: [
-      'rgba(255, 99, 132, 0.5)',   // Color de la primera barra
-      'rgba(54, 162, 235, 0.5)',  // Color de la segunda barra
-      'rgba(255, 206, 86, 0.5)',  // Color de la tercera barra
-      'rgba(75, 192, 192, 0.5)',  // Color de la cuarta barra
-      'rgba(153, 102, 255, 0.5)', // Color de la quinta barra
-    ], },
-  ];
+  public barChartData: { data: number[]; label: string; backgroundColor: string[] }[] = [
+    { data: [], label: 'Cantidad En Base A La Unidad Medida', backgroundColor: [] } 
+];
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
+    this.loadChartData();
   }
 
+  loadChartData() {
+    this.db.list('/VENTAS').valueChanges().subscribe((sales: any) => {
+
+      const typedSales = sales as Venta[];
+
+      let productData: { [key: string]: number } = {};
+      
+      typedSales.forEach(sale => {
+        sale.productos.forEach(product => {
+          if (!productData[product.nombre]) {
+            productData[product.nombre] = 0;
+          }
+          productData[product.nombre] += +product.cantidad;
+        });
+      });      
+
+  this.barChartLabels = Object.keys(productData);
+  this.barChartData[0].data = Object.values(productData);
+  this.barChartData[0].backgroundColor = this.barChartLabels.map((_, index) => this.COLORS[index % this.COLORS.length]);
+  });
+
+  }
 }
